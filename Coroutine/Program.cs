d7d8c70f950a5147ffeff09209ca305aab3f5596
Administrator <Administrator@PHENIX>
2016-07-01 15:50:08 +0800

    clean code

--- Coroutine/Program.cs
+++ Coroutine/Program.cs
@@ -2,66 +2,55 @@
 
 namespace Coroutine
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            new CoroutineTask((prevResult, onComplete,onError) =>
+            new CoroutineTask((previousTask, onComplete, onError) =>
             {
                 Console.WriteLine("1");
-              
                 onComplete(1);
             })
-            .Then((prevResult, onComplete, onError) =>
+            .Then((previousTask, onComplete, onError) =>
             {
-              
-                var x = prevResult.Result.ToString();
-                Console.WriteLine("2-"+x);
+                var result = previousTask.Result.ToString();
+                Console.WriteLine("2-" + result);
 
                 new CoroutineTask((a, b, c) =>
                 {
                     Console.WriteLine("a");
-                   
                     b("a");
                 })
-               .Then((a, b,c) =>
+                .Then((a, b, c) =>
                 {
                     Console.WriteLine("b");
                     b("b");
                 })
-                .Start(onComplete,onError);
-
+                .Start(onComplete, onError);
             })
-            .Then((prevResult, onComplete, onError) =>
+            .Then((previousTask, onComplete, onError) =>
             {
-                var y = prevResult.Result.ToString();
-                Console.WriteLine("3-"+y);
-                new CoroutineTask((a, b,c) =>
+                var result = previousTask.Result.ToString();
+                Console.WriteLine("3-" + result);
+                new CoroutineTask((a, b, c) =>
                 {
                     Console.WriteLine("aa");
                     b("aa");
                 })
-               .Start(onComplete,onError);
+                .Start(onComplete, onError);
             })
-            .Then((prevResult, onComplete, onError) =>
+            .Then((previousTask, onComplete, onError) =>
             {
-                var z= prevResult.Result.ToString();
-               
-                Console.WriteLine("4-"+z);
+                var z = previousTask.Result.ToString();
+
+                Console.WriteLine("4-" + z);
                 onComplete(4);
             })
-            .Catch((e) =>
-            {
-                Console.WriteLine("throw a exception:"+e.Message);
-            })
-            .Finally(() =>
-            {
-                Console.WriteLine("end");
-            })
+            .Catch(e => { Console.WriteLine("throw a exception:" + e.Message); })
+            .Finally(() => { Console.WriteLine("end"); })
             .Start();
 
             Console.ReadKey();
         }
-
     }
 }
